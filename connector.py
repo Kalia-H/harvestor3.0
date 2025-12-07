808c132c17e6027acbce5d820c9509a81ca25bd2
Kalia Hudson <hudsonkalia4@gmail.com>
2025-12-06 21:11:22 -0500

    Added corrected file

---
+++ connector.py
@@ -0,0 +1,9 @@
+#Name: Kalia Hudson
+#Date: 12-6-25
+#Program Name: Harvestor 3.0
+#Program Description: This application is an extension of harvestor a web scrapper.
+#This version introduces proxie rotation and image handling while maintaining
+#the previous gui from version 2.0.
+
+#File Name: connector.py
+#File Description: This file acts as a connector for the front and back end.
