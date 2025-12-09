672397e5ef5516aff9b185c29b9a08cce57e4bbb
Kalia Hudson <hudsonkalia4@gmail.com>
2025-12-09 14:01:43 -0500

    (Feat)Round-Robin referrers and agents

--- config/headers.py
+++ config/headers.py
@@ -8,3 +8,30 @@
 #File Name: headers.py
 #File Description: This file contains dynamic header configurations for requests.
 
+#creating an array of user agents to rotate through
+user_agents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (iPad; CPU OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1"
+]
+
+#creating an array of referers to rotate through
+referers = [
+    "https://www.google.com/",
+    "https://www.bing.com/",
+    "https://www.yahoo.com/",
+    "https://www.duckduckgo.com/",
+    "https://www.ask.com/"
+]
+
+#Function to get a "random" user agent
+def get_random_agent(index, user_agents):
+    length = len(user_agents)
+    return user_agents[index % length]
+    
+#Function to get a "random" referer
+def get_random_referer(index, referers):
+    length = len(referers)
+    return referers[index % length]
