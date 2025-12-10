c8d6cbe54e1efae67949f34f4d4672d4e529e1c4
Kalia Hudson <hudsonkalia4@gmail.com>
2025-12-10 13:40:32 -0500

    Added initial skeleton functions with placeholder print statements to establish the base structure in connector.py

--- connector.py
+++ connector.py
@@ -10,3 +10,29 @@
 
 #Creating a variable to hold the number of requests made
 request_count = 0
+
+import logic.engine as core
+import gui.interface as face
+from gui.interface import mainWindow
+import config.headers as headers
+import config.ips as ips
+import tkinter as tk
+
+#FUNCTION - Logic Flow
+def run_scraper(window, request_count):
+    ###TEST
+    print("Run_scraper called")
+
+#FUNCTION - Handling submit button
+def on_submit(window):  
+    ####TEST  
+    print("on_submit called")
+
+#FUNCTION - handling export button
+def on_export(window):
+    ###TEST
+    print("on_export called")
+
+#creating an instance of the mainWindow class from interface.py
+active_window = mainWindow()
+
