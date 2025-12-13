22d60da05b7790d2c390334f9506967e502ac82d
Kalia Hudson <hudsonkalia4@gmail.com>
2025-12-13 16:25:28 -0500

    Added proxie rotation logic

--- config/ips.py
+++ config/ips.py
@@ -8,12 +8,24 @@
 #File Name: ips.py
 #File Description: This file contains proxy IP configurations for requests.
 
-#creating an array of ips.
-ips = []
+#creating a list of proxy URLs for rotation
+ips = [
+    "http://108.162.192.116:80",
+    "http://108.162.192.10:80",
+    "http://108.162.192.147:80",
+    "http://108.162.192.12:80",
+]
+
 
 #Function to get a "random" ip
 def get_random_ip(index, ips):
     length = len(ips)
     return ips[index % length]
+
+#Function to create a proxie dict
+def get_proxie_dict(ip):
+    return {
+        "http": ip
+    }
 
 
